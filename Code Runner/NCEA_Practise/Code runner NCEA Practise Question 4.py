89858533d3e3cc1da3da7405e83baf0ab9a787f0
TheBro28 <hamishwenham@riccarton.school.nz>
2025-03-21 10:14:23 +1300

    Fixed NCEA Practise Question 4 and added comments

--- Code Runner/NCEA_Practise/Code runner NCEA Practise Question 4.py
+++ Code Runner/NCEA_Practise/Code runner NCEA Practise Question 4.py
@@ -1,33 +1,39 @@
-"""This code asks the user to input a score"""
-# 
+"""This code asks the user to input a score, it will then store the scores in a list and print out the amount of smart students in the class."""
+
+# Used constants to store the pass mark, top score, one, and zero.
 PASS = 80
 TOP_SCORE = 100
 ZERO = 0
-ONE_SMART_STUDENT = 1
+ONE = 1
 
-# 
+# Used a list to store all the scores the user inputs.
 scores = []
 
-# 
+# Used a while true loop to repeat the code forever untill the user inputs 'done'.
+# Used a try to test code for invalid inputs.
+# Asks the user to input a score, if the score is over or equal to 0 and under or equal to 100, it will add the score to the list. If the user inputs a value that is less than 0 it will print "Please type a positive number."
 while True:
     try:
         score = input("Enter a score, or type 'done' to exit: ")
         if score == 'done':
             break
         score = int(score)
-        if score >= PASS and score <= TOP_SCORE:
+        if score >= ZERO and score <= TOP_SCORE:
             scores.append(score)
         elif score < ZERO:
             print("Please type a positive number.")
     except ValueError:
-        print("Please type a number.")
+        print("Invalid score!")
 
-#
+# Checks if the score is over or equal to the pass mark, if it is, it will add one to the smart_students variable.
+smart_students = ZERO
 for score in scores:
-    if len(scores) == ONE_SMART_STUDENT:
-        print(f"This class has 1 smart student!")
-        break
-    else:
-        print(f"This class has {len(scores)} smart students!")
-        break
+   if score >= PASS:
+        smart_students += ONE
+
+# Prints out the amount of smart students in the class.
+if smart_students == ONE:
+    print("This class has 1 smart student!")
+else:
+    print(f"This class has {smart_students} smart students!")
 
