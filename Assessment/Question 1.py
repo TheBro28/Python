91fae9411acda0f8c96e23d58a40d8c99dc885ea
TheBro28 <hamishwenham@riccarton.school.nz>
2025-03-25 09:16:29 +1300

    Added block to calculate average sea level

--- Assessment/Question 1.py
+++ Assessment/Question 1.py
@@ -4,18 +4,24 @@
 sea_level_list = []
 
 # Constants
-
+STOP_CODE = "terminate"
+ZERO = 0
 
 # Loop
 while True:
     try:
         sea_level = input("Enter a sea level: ")
-        if sea_level == "terminate":
+        if sea_level == STOP_CODE:
             break
         sea_level = float(sea_level)
-        if sea_level < 0:
+        if sea_level < ZERO:
             print("Please enter a positive number")
         else:
             sea_level_list.append(sea_level)
     except ValueError:
         print("Invalid!")
+
+# Calculate average
+for sea_level in sea_level_list:
+    average = sum(sea_level_list) / len(sea_level_list)
+print(f"The average sea level is {average}.")
