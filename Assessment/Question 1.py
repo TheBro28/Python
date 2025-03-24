d5813d2d916eae669df8bb436ab8f698a189b1c3
TheBro28 <hamishwenham@riccarton.school.nz>
2025-03-25 09:09:25 +1300

    Added first block of code and list

--- Assessment/Question 1.py
+++ Assessment/Question 1.py
@@ -1,1 +1,21 @@
-""""""
+"""This code keeps asking the user to enter a sea level until the user types "terminate". The code will then find the average sea level based on those inputs"""
+
+# List
+sea_level_list = []
+
+# Constants
+
+
+# Loop
+while True:
+    try:
+        sea_level = input("Enter a sea level: ")
+        if sea_level == "terminate":
+            break
+        sea_level = float(sea_level)
+        if sea_level < 0:
+            print("Please enter a positive number")
+        else:
+            sea_level_list.append(sea_level)
+    except ValueError:
+        print("Invalid!")
