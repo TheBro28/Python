e27dc228a1bb7547562c83cbd37609e1fc4eadf1
TheBro28 <hamishwenham@riccarton.school.nz>
2025-03-26 14:15:22 +1300

    Fixed spelling but still apparently wrong

--- Assessment/Question 1.py
+++ Assessment/Question 1.py
@@ -1,40 +1,47 @@
 """This code keeps asking the user to enter a sea level until the user types "terminate". The code will then find the average sea level based on those inputs"""
 
-# LISTS
 # Lists used to keep track of all the user entered inputs that are equal to or above 0 (sea_levels), and a list to store all the values above the average value (above_average).
 sea_levels = []
 above_average = []
 
-# CONSTANTS
 # Constants used to remove any sort of magic number/string in the program, allowing for easy changes in future.
 STOP_CODE = "terminate"
 ZERO = 0
+ONE_ABOVE_AVERAGE = 1
 
-# LOOP
 # Loops the code forever until the user inputs "terminate".
 while True:
  # Used a "try:" to test the code for errors.
     try:
-        sea_level = input("Enter a sea level: ")
+ # Asks the user for an input, if the input is "terminate it will break the loop. 
+ # If that condition is not met then it changes the input to a float, causing any other strings that aren't "terminate" to be picked up Except ValueError block.
+ # If the input is a negitave number it will ask the user to input a positive number.
+  # If the input is then found to be a number it will add the input to the "sea_levels" list.
+        sea_level = input("Enter sea level: ")
         if sea_level == STOP_CODE:
             break
         sea_level = float(sea_level)
         if sea_level < ZERO:
-            print("Please enter a positive number")
+            print("Please enter a positive number!")
         else:
             sea_levels.append(sea_level)
+# Value Error picks up any string inputs that aren't "terminate", outputs invalid, then repeats the question.
     except ValueError:
         print("Invalid!")
 
-# Calculate average
+# Used a for loop to go through all the appended values in the "sea_levels" list.
+# Calculates the average by adding the sum of all the numbers in the list "sum(sea_levels)", divided by how many items are in the list "len(sea_levels)". Prints out the average sea level.
 for sea_level in sea_levels:
     average = sum(sea_levels) / len(sea_levels)
 print(f"The average sea level is {average}.")
 
-# 
+# For loop goes through all the sea levels in the "sea_levels" list. If the a sea level in that list is greater than the average sea level then that value will be added to the "above_average" list.
+# Prints out the "List of sea levels above average: " only if the above-average list has one or more values in it, then leaves a line and prints the list of above average sea_levels connecting them with ", ". Used the join function to join them, the map function and "str, above_average" to apply this to all items in the list.
+
 for sea_level in sea_levels:
     if sea_level > average:
         above_average.append(sea_level)
-print("List of sea levels above average: ")
-print(", ".join(map(str, above_average)))
-    
+if len(above_average) >= ONE_ABOVE_AVERAGE:
+    print("List of sea levels above average: ")
+    print(", ".join(map(str, above_average)))
+break
